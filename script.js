ea4dc0ca5633cdf6c376440ee17840cdbbbf1366
Anh Nguyen <50138935+jiwooming9@users.noreply.github.com>
2025-03-23 23:36:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', function() {
             params.append('callback', callback);
             
             // Thêm script vào document
-            script.src = 'https://script.google.com/macros/s/AKfycbzA5PEC-TXs9m3hBY4iO4RtrCRq2yI2Yf-ihojoyTYJpQWWBoCnndFsF7xHGB-r01LW/exec?' + params.toString();
+            script.src = 'https://script.google.com/macros/s/AKfycbwNX3TmzzoU77XsguVohUzzCmBDdQEN7DLIv6wt_AyJ_oON6ZM5lEGSgUSipBd-k_wu/exec?' + params.toString();
             document.body.appendChild(script);
             
             // Xử lý lỗi
